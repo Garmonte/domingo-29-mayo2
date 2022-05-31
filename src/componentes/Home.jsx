44ae828bd38715ea5f7a4a9aaeb2a6c9b921f752
Garmonte <garferark82@gmail.com>
2022-05-31 19:53:35 +0200

    Cambios dia 31

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import Link from 'react-router-dom'
 export default function Home() {
   return (
     <div className="d-flex flex-column justify-content-between container fullscreen">
-      <header>Cabecera</header>
+      <header>
+        <ul>
+          <li><Link to="/clientes">Clientes</Link></li>
+          <li><Link to="/productos">Productos</Link></li>
+          <li><Link to="/facturas">Facturas</Link></li>
+        </ul>
+      </header>
       <main className='flex-grow-1'>
         principal
       </main>
